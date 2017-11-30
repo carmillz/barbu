95bd8e3f197af155e765e1af9e78ec0d7658a163
carmillz <34136482+carmillz@users.noreply.github.com>
2017-11-30 19:08:21 +0100

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -5,6 +5,9 @@ import java.util.*;
 public class Jeu {
     ArrayList<Joueur> joueurs;
     ArrayList<Carte> plateau;
+    int nbcartes;
+    int nbdames;
+    int nbcoeurs;
     
     
     //CONSTRUCTEUR
@@ -12,6 +15,9 @@ public class Jeu {
    	 super();
    	 this.joueurs = new ArrayList<Joueur>();
          this.plateau = new ArrayList<Carte>();
+         this.nbcartes = 0;
+         this.nbcoeurs = 0;
+         this.nbdames = 0;
     }
     ///MÉTHODES
     
@@ -36,38 +42,69 @@ public class Jeu {
      */
     
     public void joueurSuivant(){
+        // un for dans le main ou pas ??????
+    }
+    
+    /*
+     * Exécution d'une partie
+    */
+    public void partie(ArrayList<Joueur> joueurs){
+        boolean finPartie=false;
+        while (!finPartie){
+            boolean finManche=false;
+            while (!finManche) {
+                for (int i=0; i<joueurs.size(); i++){
+                    joueurs.get(i).choisirCarte(this);
+                }
+                // regarder qui a gagné
+                /* on change la valeur de finManche si :
+                 * les joueurs n'ont plus de cartes
+                 * suivant les contrats
+                 *
+                */
+            }
+        }
         
     }
     
+    public void creationJoueurs(){
+        boolean creation=true;
+        while (!creation){
+            int i=1 ;
+            Scanner sc= new Scanner (System.in);
+            System.out.println("Entrez le nom du joueur");
+            String nom = sc.nextLine();
+            joueurs.add(new Joueur(nom,String.valueOf(i)));
+            i++;
+            System.out.println("Y-a-t-il un autre joueur ? (true/false)");
+            creation = sc.nextBoolean();
+            
+        }
+    }
+    
     public static void main (String[] args) {
-   	 Paquet paquet = new Paquet(52);
-   	 Jeu jeu = new Jeu();
-   	 //demander le nombre de joueurs
-   	 //demander le nombre de cartes
-   	 Joueur j1 = new Joueur("1","");
-   	 Joueur j2 = new Joueur("2","");
-         Joueur j3 = new Joueur("3","");
-         Joueur j4 = new Joueur("4","");
-   	 jeu.joueurs.add(j1);
-   	 jeu.joueurs.add(j2);
-         jeu.joueurs.add(j3);
-         jeu.joueurs.add(j4);
-   	 //System.out.println("Paquet départ"+paquet.paquet.size());
-   	 paquet.distribuer(jeu.joueurs);
-         jeu.joueurs.get(0).choisirCarte(jeu);
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Voulez-vous jouer avec 32 ou 52 cartes ?"); //demande le nombre de cartes
+        int nbcartes = sc.nextInt();
+   	Paquet paquet = new Paquet(nbcartes);
+   	Jeu jeu = new Jeu(); // initialise le jeu
+        jeu.creationJoueurs(); //initialise les jouers
+        paquet.cartesAJouer(jeu.joueurs.size()); // adapte le nombre de cartes en fonction du nombre de joueus
+        paquet.distribuer(jeu.joueurs); //distribue les cartes entre les joueurs
+        jeu.rotation();
+        jeu.partie(jeu.joueurs);
+         /*
          for (int i=0; i<jeu.joueurs.get(0).getMain().size(); i++){
                 System.out.println(i + " : " + jeu.joueurs.get(0).getMain().get(i).toString());
           } 
+        */
+         
    	 //System.out.println(paquet.paquet.size());
    	 //System.out.println("Main :"+ j1.getMain().toString());
    	 //System.out.println(j1.getMain().size());
-   	 jeu.rotation();
-   	 jeu.rotation();
    	 ;
     }
 
 
 }
-
-
 
