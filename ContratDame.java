f2bf31409d8b598fc2f8b85fd8e8de40946656c0
Hypnosi <34155223+Hypnosi@users.noreply.github.com>
2017-12-02 21:39:19 +0100

    Update ContratDame.java

--- ContratDame.java
+++ ContratDame.java
@@ -1,20 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package barbu;
 
-/**
- *
- * @author Salmé
- */
-public class ContratDame implements Contrat{
-    
-    public boolean fin(Jeu jeu){
-        if (jeu.nbcartes==7){
-            return true;
-        }
-        return false;
-    }
+public class ContratDame implements Contrat {
+
+	public ContratDame(Jeu jeu) {
+		jeu.partie(jeu.joueurs);
+	}
+	public boolean fin(Jeu jeu) {
+		if (jeu.nbdames == 4) {
+			return true;
+		}
+		return false;
+	}
+
+
+	public int comptePoints(Joueur joueur) {
+		int cpt =0;
+		while (!joueur.getPlis().isEmpty()) {
+			Carte c = joueur.getPlis().iterator().next();
+			if (c.getValeur()==13) {
+				cpt=cpt+1;
+			}
+			joueur.getPlis().remove(c);
+		}
+		return (cpt*20);
+		
+	}
+
+//Methode non utilisée
+
+	@Override
+	public boolean fin(Jeu jeu, int nombre) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
