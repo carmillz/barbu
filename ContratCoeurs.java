c9a80ab16a0e23a2b8e4cec803a8d2290f6d686c
CCassandre <cassandre.chipot@outlook.fr>
2017-12-03 14:58:02 +0100

    Erreur avant : c'est celui là où il n'y avait pas la bonne extension

--- ContratCoeurs.java
+++ ContratCoeurs.java
@@ -1,13 +1,14 @@
 package barbu;
 
+
 public class ContratCoeurs  implements Contrat {
-	
+
 	public ContratCoeurs(Jeu jeu) {
 		jeu.partie(jeu.joueurs);
 	}
 
 	public boolean fin(Jeu jeu, int nombre) {
-		if ( nombre==32) {	//on vérifie le nmobre de cartes pour la partie 
+		if ( nombre==32) {	//on vérifie le nmobre de cartes pour la partie
 			if (jeu.nbcoeurs == 8) { // on compte le nombre de coeurs en fonction du nombre total de cartes
 				return true;
 			}
@@ -29,7 +30,7 @@ public class ContratCoeurs  implements Contrat {
 			joueur.getPlis().remove(c);
 		}
 		return (cpt*10);
-		
+
 	}
 
 //non utilisé
