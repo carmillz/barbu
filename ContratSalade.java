3329b9369b16d5a54599815ce668c53f74a8f001
OuafaB <ouafa54440@gmail.com>
2017-12-14 18:41:09 +0100

    Add files via upload

--- ContratSalade.java
+++ ContratSalade.java
@@ -3,25 +3,31 @@ package barbu;
 public class ContratSalade implements Contrat{
 
 	public ContratSalade(Jeu jeu) {
-		jeu.partie(new ContratSalade());
-	}
-
-
-	public ContratSalade() {
 		// TODO Auto-generated constructor stub
 	}
 
 
 	@Override
 	public int comptePoints(Joueur joueur) {
-		// TODO Auto-generated method stub
-		return 0;
+            int cpt =0;
+            while (!joueur.getPlis().isEmpty()) { //Tant que le joueur a encore des plis on compte ses points
+                Carte c = joueur.getPlis().iterator().next();
+                if (){
+                    
+                }
+            }
+            joueur.getPlis().remove(c);
+             
+            return 0;
 	}
 
 	@Override
 	public boolean fin(Jeu jeu, int nombre) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+            if (jeu.nbcartesencours == 0) { // Si le nombre de carte en jeu est égal à 0 (donc plus de carte à jouer)
+                return true;         // Alors la partie est finie 
+            }
+                      
+            return false;            // Sinon la partie continue 
+		
 
 }
