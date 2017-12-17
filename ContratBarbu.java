1287864414c86e871f562ba600214ddf198ba761
Hypnosi <34155223+Hypnosi@users.noreply.github.com>
2017-12-17 20:55:57 +0100

    Update ContratBarbu.java

--- ContratBarbu.java
+++ ContratBarbu.java
@@ -5,16 +5,15 @@ import java.util.ArrayList;
 public class ContratBarbu implements Contrat {
 
     public ContratBarbu(Jeu jeu) {
-        jeu.partie(new ContratBarbu());
+        new ContratBarbu();
     }
 
     public ContratBarbu() {
 
     }
 
-    public boolean fin(Jeu jeu, int nombre) {
-
-        if (jeu.plateau.contains(new Carte("Coeur", 14))) {
+    public boolean fin(int nombre) {
+        if (nombre==1) {
             return true;
         }
         return false;
@@ -22,17 +21,18 @@ public class ContratBarbu implements Contrat {
 
     @Override
     public void comptePoints(ArrayList<Joueur> joueurs) {
-        for (int i = 0; i <= joueurs.size(); i++) {
-            Joueur joueur = joueurs.get(i);
+        for (int i = 0; i < joueurs.size(); i++) {
             int cpt = 0;
-            while (!joueur.getPlis().isEmpty()) {
-                Carte c = joueur.getPlis().iterator().next();
-                if (c.getValeur() == 14 && c.getSymbole().equals("Coeur")) {
+            while (!joueurs.get(i).getPlis().isEmpty()) {
+                Carte c = joueurs.get(i).getPlis().iterator().next();
+                if (c.getValeur() == 13 && c.getSymbole().equals("Coeur")) {
                     cpt++;
                 }
-                joueur.getPlis().remove(c);
-                joueur.getPoints().add(cpt * 80);
+                joueurs.get(i).getPlis().remove(c);
+                System.out.println(cpt);
+                
             }
+            joueurs.get(i).getPoints().add(cpt * 80);
         }
     }
 }
