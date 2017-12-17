d913ab7c630a1001b0459c968759a0656e937620
Hypnosi <34155223+Hypnosi@users.noreply.github.com>
2017-12-17 20:56:39 +0100

    Update ContratDame.java

--- ContratDame.java
+++ ContratDame.java
@@ -6,36 +6,35 @@ import java.util.ArrayList;
 public class ContratDame implements Contrat {
 
 	public ContratDame(Jeu jeu) {
-		jeu.partie(new ContratDame());
+		new ContratDame();
 	}
+
 	public ContratDame() {
-		// TODO Auto-generated constructor stub
 	}
-	public boolean fin(Jeu jeu, int nombre) {
-		if (jeu.nbdames == 4) {
+
+	public boolean fin(int nombre) {
+		if (nombre == 4) {
 			return true;
-		          }
+		}
 		return false;
 	}
 
-
 	public void comptePoints(ArrayList<Joueur> joueurs) {
-		
-                for (int i=0; i<=joueurs.size();i++){
-                    int cpt =0;
-                    Joueur joueur = joueurs.get(i);
-                    while (!joueur.getPlis().isEmpty()) {
-                        Carte c = joueur.getPlis().iterator().next();
-                        if (c.getValeur()==13) {
-                                cpt=cpt+1;
-                        }
-                        joueur.getPlis().remove(c);
-                        joueur.getPoints().add(cpt*20);
-                    }
-                }
-		
+
+		for (int i = 0; i < joueurs.size(); i++) {
+			int cpt = 0;
+			Joueur joueur = joueurs.get(i);
+			while (!joueur.getPlis().isEmpty()) {
+				Carte c = joueur.getPlis().iterator().next();
+				if (c.getValeur() == 12) {
+					cpt = cpt + 1;
+				}
+				joueur.getPlis().remove(c);
+				
+			}
+			joueur.getPoints().add(cpt * 20);
+		}
+
 	}
-
-
 
 }
