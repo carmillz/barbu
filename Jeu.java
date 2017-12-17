6284ca6587fe3d2e3bce340b1aad2060fd3f600c
Hypnosi <34155223+Hypnosi@users.noreply.github.com>
2017-12-17 20:58:26 +0100

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -5,23 +5,24 @@ import java.util.*;
 public class Jeu {
 	ArrayList<Joueur> joueurs;
 	Collection<Carte> plateau;
-	int nbcartesencours;
-	int nbcartes; // utile pour le contrat pli
-	int nbdames; // utile pour le contrat dame
-	int nbcoeurs; // utilie pour le contrat coeur
+	int nbCartes; // utile pour le contrat pli
+	int nbDames; // utile pour le contrat dame
+	int nbCoeurs; // utile pour le contrat coeur
+	int nbRoi; // utile pour le contrat roi
+	int nbCartesReel;
 
 	// CONSTRUCTEUR
-	public Jeu(int nbcartes) {
+	public Jeu(int nbCartes) {
 		super();
 		this.joueurs = new ArrayList<Joueur>();
 		this.plateau = new ArrayList<Carte>();
-		this.nbcartesencours = nbcartes;
-		this.nbcartes = nbcartes;
-		this.nbcoeurs = 0;
-		this.nbdames = 0;
+		this.nbCartes = nbCartes;
+		this.nbCoeurs = 0;
+		this.nbDames = 0;
+		this.nbCartesReel=nbCartes;
 	}
 
-	// Mà‰THODES
+	// MÉTHODES
 
 	/*
 	 * Méthode qui va choisir un joueur (le dernier de notre arraylist de joueurs)
@@ -31,97 +32,140 @@ public class Jeu {
 	public void rotation() {
 		int last = joueurs.size() - 1; // donne la position du dernier joueur
 		System.out.println("Le joueur " + joueurs.get(last).toString() + " distribue les cartes");
-		System.out.println("Le joueur " + joueurs.get(0).toString() + " lance la partie");
+		System.out.println("Le joueur " + joueurs.get(0).toString() + " choisit le contrat et lance la partie");
 		// sens inverse des aiguilles d'une montre
 		joueurs.add(joueurs.get(0));
 		joueurs.remove(0);
 		System.out.println(joueurs.toString());
 	}
-	
+
 	/*
-	 * Méthode qui permet d'ajouter au joueur gangnant le pli en cours
-	 * Une fois les quatre cartes "posées" sur le plateau, on les 
-	 * compare avec les autres via une boucle qui permettra de rendre
-	 * la carte la "plus forte" sachant que cette dernià¨re doit
-	 * possà¨der le màªme symboel que la premià¨re carte jouée.
-	 * Une fois que cette carte est trouvée, on l'ajoute à  l'attribut 
-	 * qui conserve les plis du joueur gagnant
+	 * Méthode qui permet d'ajouter au joueur gagnant le pli en cours Une fois les
+	 * quatre cartes "posées" sur le plateau, on les compare avec les autres via une
+	 * boucle qui permettra de rendre la carte la "plus forte" sachant que cette
+	 * dernière doit possèder le màªme symboel que la première carte jouée. Une
+	 * fois que cette carte est trouvée, on l'ajoute à  l'attribut qui conserve les
+	 * plis du joueur gagnant
 	 */
-	public void gagnant() { 
+	public void gagnant() {
 		String symbole = ((ArrayList<Carte>) plateau).get(0).getSymbole();
 		int valeur = ((ArrayList<Carte>) plateau).get(0).getValeur();
 		int max = 0;
-		for (int i = 1; i < joueurs.size(); i++) { 
-			if (((ArrayList<Carte>) plateau).get(i).getSymbole().equals(symbole) && ((ArrayList<Carte>) plateau).get(i).getValeur() > valeur) {
+		for (int i = 1; i < joueurs.size(); i++) {
+			if (((ArrayList<Carte>) plateau).get(i).getSymbole().equals(symbole)
+					&& ((ArrayList<Carte>) plateau).get(i).getValeur() > valeur) {
 				valeur = ((ArrayList<Carte>) plateau).get(i).getValeur();
-				max = i; 
+				max = i;
 			}
 		}
 		System.out.println("Le gagnant de ce pli est " + joueurs.get(max).getNom());
-		joueurs.get(max).setPlis(new HashSet<Carte>(plateau));
+		joueurs.get(max).getPlis().addAll(new HashSet<Carte>(plateau));
 	}
 
+	public boolean fin(Contrat contrat) {
+		if (contrat.getClass().toString().equals("class barbu.ContratBarbu")) {
+			return contrat.fin(nbRoi);
+		} else if (contrat.getClass().toString().equals("class barbu.ContratCoeurs")) {
+			return contrat.fin(nbCoeurs);
+		} else if (contrat.getClass().toString().equals("class barbu.ContratDame")) {
+			return contrat.fin(nbDames);
+		}
+		return false;
+	}
 	/*
 	 * Exécution d'une partie
 	 */
-	
-	public void partie(Contrat contrat){
+
+	public void partie() {
+		if (joueurs.size()==3 && nbCartes==32) {
+			nbCartesReel =30;
+		}
+		if (joueurs.size()==3 && nbCartes==52) {
+			nbCartesReel=51;
+		}
+		Paquet paquet ;
+		Contrat contrat = this.choixContrat();
 		boolean finPartie = false;
 		while (!finPartie) {
-			int cartesJouees = 0;
-			while (cartesJouees<nbcartes) {
-				cartesJouees += manche();
-				contrat.comptePoints(joueurs);
+			paquet = new Paquet(nbCartes, this.joueurs.size());
+			paquet.distribuer(this.joueurs); // distribue les cartes entre les joueurs
+			this.rotation();
+			manche(contrat);
+			contrat.comptePoints(joueurs);
+			// il faut vider les mains des joueurs si la manche s'est arrêtée avant de vider cette dernière
+			for (int i=0; i < joueurs.size(); i++) {
+				joueurs.get(i).setMain(new ArrayList<Carte>()) ;
+			}
+			System.out.println("Les points gagnés lors de cette manche sont :");
+			for (int i = 0; i < joueurs.size(); i++) {
+				System.out.println("Le joueur " + joueurs.get(i).getNom() + " a " + joueurs.get(i).getPoints());
+			}
+			contrat = choixContrat();
+			if (contrat == null) {
+				finPartie = true;
+			} else {
+				partie();
 			}
 		}
 	}
-	
-	
-	public int manche(){
-		int cartesJouees=0;
-		for (int i=1; i < nbcartes/joueurs.size(); i++){
-			cartesJouees = this.tour();
+
+	public void manche(Contrat contrat) {
+		int cartesJouees = 0;
+		while (cartesJouees < nbCartesReel) {
+			cartesJouees += this.tour();
+			if (this.fin(contrat)) {
+				cartesJouees = nbCartesReel;
+
+			}
 			gagnant();
-			plateau=new ArrayList<Carte>();
+			plateau = new ArrayList<Carte>();
 		}
-		return cartesJouees;
 	}
-	
-	public int tour(){
-		for (int j=0; j<joueurs.size(); j++){
+
+	public int tour() {
+		for (int j = 0; j < joueurs.size(); j++) {
 			System.out.println("C'est au tour de " + joueurs.get(j).getNom() + " de jouer ");
 			System.out.println(" Les cartes déjà  jouées sont : ");
-			for (int k=0; k<this.plateau.size(); k++){
+			for (int k = 0; k < this.plateau.size(); k++) {
 				System.out.println(((ArrayList<Carte>) plateau).get(k) + " jouée par " + joueurs.get(k).getNom());
 			}
-			joueurs.get(j).choisirCarte(this,plateau);
+			joueurs.get(j).choisirCarte(this, plateau);
 		}
 		System.out.println(" Les cartes jouées lors de ce tour sont : ");
-		for (int k=0; k<this.plateau.size(); k++){
+		for (int k = 0; k < this.plateau.size(); k++) {
 			System.out.println(((ArrayList<Carte>) plateau).get(k) + " jouée par " + joueurs.get(k).getNom());
+			if (((ArrayList<Carte>) plateau).get(k).getSymbole().equals("Coeur")) {
+				nbCoeurs++;
+				if (((ArrayList<Carte>) plateau).get(k).getValeur() == 13) {
+					nbRoi++;
+				}
+			}
+			if (((ArrayList<Carte>) plateau).get(k).getValeur() == 12) {
+				nbDames++;
+			}
 		}
+
 		return plateau.size();
 	}
-	
-	
-	public void creationJoueurs() throws Exception{
+
+	public void creationJoueurs() throws Exception {
 		boolean creation = true;
 		Scanner sc = new Scanner(System.in);
 		int i = 1;
 		while (creation) {
 			System.out.println("Entrez le nom du joueur ou faites entrer s'il n'y en a plus");
 			String nom = sc.nextLine();
-			if (nom.equals("")){
-				creation=false;
-			}else {
-				joueurs.add(new Joueur(nom,String.valueOf(i)));
+			if (nom.equals("")) {
+				creation = false;
+			} else {
+				joueurs.add(new Joueur(nom, String.valueOf(i)));
 			}
 			i++;
 		}
-		if (joueurs.size()<=1){
+		if (joueurs.size() <= 1) {
 			throw new Exception("Vous ne pouvez pas jouer tout seul !");
 		}
-		//sc.close();
+		// sc.close();
 	}
 
 	// Méthode qui demande à  l'utilisateur le contrat qu'il veut
@@ -137,52 +181,51 @@ public class Jeu {
 			System.out.println("4 : Le barbu (ou le roi de Coeur)");
 			System.out.println("5 : La Réussite");
 			System.out.println("6 : La Salade");
-			System.out.println("0 : Quitter");
+			System.out.println("7 : Quitter");
 			tmp = sc.nextInt();
-			if (!quitter){
+			if (!quitter) {
 				int choix = Integer.valueOf(tmp);
 				switch (choix) {
-					case 1:
-						return new ContratPli(this);
-					case 2:
-						return new ContratDame(this);
-					case 3:
-						return new ContratCoeurs(this);
-					case 4:
-						return new ContratBarbu(this);
-					case 5:
-						return new ContratReussite(this);
-					case 6:
-						return new ContratSalade(this);
-					case 0:
-						quitter=true;
-						break;
-					default:
-						System.out.println("Option inexistante, recommencez");
-						break;
-					}
-				//sc.close();
+				case 1:
+					return new ContratPli(this);
+				case 2:
+					return new ContratDame(this);
+				case 3:
+					return new ContratCoeurs(this);
+				case 4:
+					return new ContratBarbu(this);
+				case 5:
+					return new ContratReussite(this);
+				case 6:
+					return new ContratSalade(this);
+				case 7:
+					quitter = true;
+					return null;
+				default:
+					System.out.println("Option inexistante, recommencez");
+					break;
 				}
+				// sc.close();
 			}
-		return null; // DEMANDER A CASSANDRE
+		}
+		return null;
 	}
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Voulez-vous jouer avec 32 ou 52 cartes ?"); // demande le nombre de cartes
-		int nbcartes = sc.nextInt();
-		
-		Jeu jeu = new Jeu(nbcartes);
+		int nbCartes = sc.nextInt();
+		Jeu jeu = new Jeu(nbCartes);
+		if (nbCartes == 32) {
+			// il faut ajuster le nombre de coeurs pour la méthode fin du contrat coeur
+			// Comme il n'y a que 8 coeurs lorsqu'on joue à 32 cartes et que la méthode
+			// fin "arrête" le jeu à 14
+			jeu.nbCoeurs = 6;
+		}
 		jeu.creationJoueurs(); // initialise les joueurs
-		Paquet paquet = new Paquet(nbcartes, jeu.joueurs.size());
-		System.out.println(paquet.toString()+ "\n" + paquet.paquet.size());
+		jeu.partie();
 
-		paquet.distribuer(jeu.joueurs); // distribue les cartes entre les joueurs
-		jeu.rotation();
-		System.out.println(jeu.nbcartes);
-		jeu.partie(jeu.choixContrat());
-
-		//sc.close();
+		// sc.close();
 		/*
 		 * for (int i=0; i<jeu.joueurs.get(0).getMain().size(); i++){
 		 * System.out.println(i + " : " +
