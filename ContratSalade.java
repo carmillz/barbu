86f24e3c6bfa380cf3c580fd1c59f08266f66d3d
Hypnosi <34155223+Hypnosi@users.noreply.github.com>
2017-12-17 20:57:36 +0100

    Update ContratSalade.java

--- ContratSalade.java
+++ ContratSalade.java
@@ -11,38 +11,35 @@ public class ContratSalade implements Contrat {
     @Override
     public void comptePoints(ArrayList<Joueur> joueurs) {
         Carte c;
-        for (int i = 0; i <= joueurs.size(); i++) {
+        for (int i = 0; i < joueurs.size(); i++) {
             int cptcoeur = 0;
             int cptdame = 0;
             int cptroi = 0;
-            int cptpli = 0;
+            int plis = joueurs.get(i).getPlis().size()/joueurs.size()* 10;
+			System.out.println(joueurs.get(i).getPlis().toString());
             Joueur joueur = joueurs.get(i); // On fait joueur par joueur, et on simplifie pour l'utilisation du joueur i plus tard
             while (!joueur.getPlis().isEmpty()) { // Tant que le joueur a des plis
                 c = joueur.getPlis().iterator().next(); // On regarde carte par carte 
-                if (c.getSymbole().equals("Coeur")) { // On compte le nombre de coeur
-                    cptcoeur+=cptcoeur;
+                if (c.getSymbole().equals("Coeur")) { // On compte le nombre de coeurs
+                    cptcoeur++;
                 }
-                if (c.getValeur()==13) { // On compte le nombre de dame
-                    cptdame+= cptdame;
+                if (c.getValeur()==12) { // On compte le nombre de dames
+                    cptdame++;
                 }
-                if (c.getValeur() == 14 && c.getSymbole().equals("Coeur")){ // On donne les points du roi si il y est
-                    cptroi= 80;
+                if (c.getValeur() == 13 && c.getSymbole().equals("Coeur")){ // On donne les points du roi si il y est
+                    cptroi++;
                 }
-                cptpli= joueurs.get(i).getNbplis() * 10; //  On donne les points pour tous les plis du joueur i
                 joueur.getPlis().remove(c); // On enlève une carte 
             }
-            joueur.getPoints().add(cptcoeur*10 + cptdame*20 + cptroi + cptpli); //Vérifier les points dans tous les contrats et celui-ci !!
+            joueur.getPoints().add(plis + cptcoeur*10 + cptdame*20 + cptroi*80); //Vérifier les points dans tous les contrats et celui-ci !!
         }
         
     }
 
-    
+    //Méthode non utilisée
     @Override
-    public boolean fin(Jeu jeu, int nombre) {
-        // Si le nombre de carte en jeu est égal à 0 (donc plus de carte à jouer) 
-        // Alors la partie est finie donc : True
-        // Sinon la partie continue donc: False
-        return jeu.nbcartesencours == 0;            
+    public boolean fin(int nombre) {
+        return false;            
     }
     
 }
