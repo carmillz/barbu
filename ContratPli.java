4b708041fac0f5d4d3de62f4f8479166b19012a4
Hypnosi <34155223+Hypnosi@users.noreply.github.com>
2017-12-17 20:57:09 +0100

    Update ContratPli.java

--- ContratPli.java
+++ ContratPli.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class ContratPli implements Contrat {
 
     public ContratPli(Jeu jeu) {
-        jeu.partie(new ContratPli());
+        new ContratPli();
     }
 
     public ContratPli() {
@@ -14,14 +14,15 @@ public class ContratPli implements Contrat {
 
     @Override
     public void comptePoints(ArrayList<Joueur> joueurs) {
-        for (int i = 0; i <= joueurs.size(); i++) {
-            joueurs.get(i).getPoints().add(joueurs.get(i).getNbplis() * 10);
+        for (int i = 0; i < joueurs.size(); i++) {
+            joueurs.get(i).getPoints().add((joueurs.get(i).getPlis().size()/joueurs.size())* 10);
         }
     }
 
     @Override
-    public boolean fin(Jeu jeu, int nombre) {
-        return jeu.nbcartesencours == 0; // Si il y a encore des cartes en jeu, alors on renvoit true pour signifier la fin
+    //Méthode non utilisée
+    public boolean fin(int nombre) {
+        return false;
     }
 
 }
